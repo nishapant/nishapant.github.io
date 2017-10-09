cc0dbe2140bacab9da57d49abeab4accdca3b74f
Nisha Pant <nisha007pant@gmail.com>
2017-10-08 19:29:02 -0500

    updated bio

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function editPic(){
-    document.getElementById('headshot').style.opacity = "1";
-    document.getElementById('headshot').style.height = "66%";
-}
-
-function changePic(){
-  document.getElementById('headshot').style.opacity = "0.8";
-  document.getElementById('headshot').style.height = "65%";
-}
+// function editPic(){
+//     document.getElementById('headshot').style.opacity = "1";
+//     document.getElementById('headshot').style.height = "66%";
+// }
+//
+// function changePic(){
+//   document.getElementById('headshot').style.opacity = "0.8";
+//   document.getElementById('headshot').style.height = "65%";
+// }
