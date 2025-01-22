1f3708a4b26b8c4fc60ab12a90927e4032c28ae2
Nisha Pant <nisha@moderntreasury.com>
2025-01-22 11:49:22 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Photos from "./Photos";
+import Projects from "./Projects";
+import Writing from "./Writing";
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/archive" element={<Photos />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/writing" element={<Writing />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
---
+++ src/Writing.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Writing() {
+  return (
+    <div>
+      <header className="flex flex-row p-5 gap-x-5 justify-center text-magenta font-bold px-15 font-birthstone text-2xl">
+        <a href="/">NP</a>
+      </header>
+    </div>
+  )
+}
+
+export default Writing;
